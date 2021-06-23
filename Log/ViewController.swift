79bedf5e3d37b1ceacb8be8ebbf3bf67503e4bda
TarasShu <65026433+TarasShu@users.noreply.github.com>
2021-06-24 01:35:25 +0300

    Initial Commit

---
+++ Log/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Log
+//
+//  Created by Taras Shukhman on 24/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
